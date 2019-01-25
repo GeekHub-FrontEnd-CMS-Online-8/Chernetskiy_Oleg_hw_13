7ddcd74fc4b8027a3f6e20120492984da3fa84b6
Oleg Chernetskiy <Moonstalker.oc@gmail.com>
2019-01-25 22:51:06 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,66 @@
+jQuery(function ($) {
+    const todos = [];
+    const list = $('.todos');
+    const input = $('input');
+
+    function addTodo(value) {
+        todos.push(value);
+        renderTodos();
+    };
+
+    function renderTodos() {
+        list.empty();
+        $.each(todos, function (i) {
+            list.prepend(`
+               <li>
+                 ${this}
+                 <button data-index="${i}" class="delete">Delete</button>
+                 <button data-index="${i}" class="edit">Edit</button>
+               </li>
+            `);
+            var tasks = JSON.stringify(todos);
+            window.localStorage.setItem('todos', tasks);
+            var tasksViev = JSON.parse(window.localStorage.getItem('todos'));
+        });
+    };
+
+    function removeTodo(index) {
+        todos.splice(index, 1);
+        renderTodos();
+    };
+
+    function editTodo (index) {
+        var elementText = todos.splice(index, 1);
+        test = prompt('Меняй меня полностю', elementText);
+        todos.push(test);
+        renderTodos();
+    };
+
+    input.on('change', function () {
+        addTodo(this.value);
+        this.value = '';
+    });
+
+    $(document).on('click', '.delete', function () {
+        const index = $(this).data('index');
+        removeTodo(index);
+    });
+
+    $(document).on('click', '.edit', function () {
+        const index = $(this).data('index');
+        editTodo(index);
+    });
+
+    $(document).on('click', '.loadMore', function () {
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/todos", success: function (result) {
+                result.splice(0, 10).map((item) => {
+                    todos.push(item.title);
+                    renderTodos();
+                })
+            }
+        })
+    });
+
+});
+
